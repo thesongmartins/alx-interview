e84c4ed8d4ae6b68659311e5058cadf2af678641
thesongmartins <martinsjehoshaphat@gmail.com>
2024-11-15 21:21:23 +0100

    API update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,19 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-    if (err) throw err;
-    const actors = JSON.parse(body).characters;
-    exactOrder(actors, 0);
-});
+// EXactOrder Function
 const exactOrder = (actors, x) => {
-    if (x === actors.length) return;
+    if (x === actors.length) return; // Base case for recursion
     request(actors[x], function (err, res, body) {
         if (err) throw err;
-        console.log(JSON.parse(body).name);
-        exactOrder(actors, x + 1);
+        console.log(JSON.parse(body).name); // Log character name
+        exactOrder(actors, x + 1); // Recursive call for next actor
     });
 };
+
+// Request films API
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+    if (err) throw err; // Handle request errors
+    const actors = JSON.parse(body).characters; // Extract characters list
+    exactOrder(actors, 0); // Start recursion with first actor
+});
